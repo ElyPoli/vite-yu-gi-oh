70e965b753f3b2532a7fc6d2b2418487ce09a995
ElyPoli <itsos.poli@gmail.com>
2023-08-24 14:56:32 +0200

    added graphics layout TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script>
-export default {
+import TheHeader from "./components/TheHeader.vue";
 
+export default {
+    components: {
+        TheHeader,
+    }
 }
 </script>
 
 <template>
+    <TheHeader></TheHeader>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header class="ps-5 pt-3 pb-3">
+        <img src="/public/img/logo-Yu-Gi-Oh.png" alt="logo-Yu-Gi-Oh" class="pe-3">
+        <h1 class="m-0 p-0 ps-3">Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+header {
+    @include box-left;
+    
+    img {
+        cursor: pointer;
+        max-width: 100px;
+    }
+
+    h1 {
+        cursor: pointer;
+        font-size: 3rem;
+    }
+}
+
+</style>
