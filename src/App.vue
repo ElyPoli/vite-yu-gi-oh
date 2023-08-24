22ee1e7630399271098131a29572eeb187a72838
ElyPoli <itsos.poli@gmail.com>
2023-08-24 16:59:23 +0200

    added graphics layout CardBox

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import CardBox from "./components/CardBox.vue";
 
 export default {
     components: {
         TheHeader,
+        CardBox,
     }
 }
 </script>
 
 <template>
     <TheHeader></TheHeader>
+    <main>
+        <div class="container pt-4 pb-4">
+            <!-- Form seleziona opzioni -->
+            <select class="form-select">
+                <option value="1">Alien</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+        <CardBox></CardBox>
+    </main>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
---
+++ src/components/CardBox.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container card-container pt-5 ps-5 pe-5 pb-2">
+        <!-- Sezione carte trovate -->
+        <div class="found-card">
+            <p class="text-white m-0 p-0 fw-bold">Found 39 cards</p>
+        </div>
+        <div class="row row-cols-5 card-box gy-4">
+            <div class="col">
+                <div class="card">
+                    <img src="/public/img/layout-da-riprodurre.png" alt="test">
+                    <div class="card-body">
+                        <h5 class="text-white text-uppercase fs-5 fw-bold">Card title</h5>
+                        <p class="card-text">tipo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+.card-container {
+    background-color: #ffffff;
+}
+
+.card-box {
+    .card {
+        cursor: pointer;
+        height: 100%;
+        color: var(--bs-body-color);
+        background-color: $primary-color;
+        border: none;
+        border-radius: 0;
+
+        .card-body {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            text-align: center;
+        }
+    }
+}
+
+.found-card {
+    background-color: $secondary-color;
+    padding: 1rem;
+}
+</style>
